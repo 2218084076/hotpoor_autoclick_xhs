8244964f38b084ac14cc825dbcfa803c38c2f54f
Terry <82017869+2218084076@users.noreply.github.com>
2021-11-29 10:03:41 +0800

    update

---
+++ mi_daniel.py
@@ -0,0 +1,51 @@
+import os
+import time
+import pyautogui
+
+n=1
+while True:
+    os.system("adb -s 869e65410721 shell input tap 989 365")
+    time.sleep(1)
+    os.system("adb -s 869e65410721 shell input swipe 528 1926 528 262 150")
+    time.sleep(2)
+    os.system("adb -s 869e65410721 shell input swipe 528 1926 528 262 150")
+    time.sleep(2)
+    os.system("adb -s 869e65410721 shell input tap 283 500")
+    time.sleep(2)
+    os.system("adb -s 869e65410721 shell input tap 1012 155")
+    time.sleep(2)
+    os.system("adb -s 869e65410721 shell input tap 324 1997")
+    pyautogui.click(1538,336)
+    time.sleep(2)
+    pyautogui.hotkey('ctrl','a')
+    time.sleep(2)
+    pyautogui.hotkey('ctrl','v')
+    time.sleep(2)
+    pyautogui.click(1400,397)
+    time.sleep(25)
+    # pyautogui.click(1536,251)
+    # time.sleep(2)
+    os.system("adb -s 869e65410721 shell input tap 71 120")
+    time.sleep(2)
+
+
+    os.system("adb -s 869e65410721 shell input tap 263 1457")
+    time.sleep(2)
+    os.system("adb -s 869e65410721 shell input tap 1012 155")
+    time.sleep(2)
+    os.system("adb -s 869e65410721 shell input tap 324 1997")
+    pyautogui.click(1538,336)
+    time.sleep(2)
+    pyautogui.hotkey('ctrl', 'a')
+    time.sleep(2)
+    pyautogui.hotkey('ctrl','v')
+    time.sleep(2)
+    pyautogui.click(1400,397)
+    time.sleep(25)
+    # pyautogui.click(1536, 251)
+    # time.sleep(2)
+    os.system("adb -s 869e65410721 shell input tap 71 120")
+    time.sleep(2)
+    os.system("adb -s 869e65410721 shell input swipe 528 1926 528 262 150")
+    print(n)
+    n+=1
