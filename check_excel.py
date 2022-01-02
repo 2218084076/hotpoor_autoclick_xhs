b0a203687faf3ef8ffae9d0f7142bfc5ee9310cd
Terry <82017869+2218084076@users.noreply.github.com>
2022-01-02 22:48:12 +0800

    Split parsing excel

---
+++ check_excel.py
@@ -0,0 +1,16 @@
+import xlrd
+filename = r"D:\github\1\hotpoor_autoclick_xhs\费用报销单模板-12月.xls"
+def reader():
+    data = xlrd.open_workbook(filename,formatting_info=True)
+    table = data.sheets()[0]
+    merged_cells=[]
+    for i in table.merged_cells:
+        merged_cells.append(list(i))
+    company = []
+    for i in range(0,table.nrows):
+        company.append(table.row_values(i))
+        print(table.row_values(i))
+    # print(company)
+    # print(merged_cells)
+reader()
+
